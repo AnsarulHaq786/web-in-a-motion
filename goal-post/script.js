3349d2ca338f23fac18952cd5f6ed468f3c48826
user.name "Ansarul Haq <mdansarul786940@gmail.com>
2026-02-14 11:24:57 +0545

    feat: position goal post and push ball to goal

---
+++ goal-post/script.js
@@ -0,0 +1,46 @@
+const tl = gsap.timeline();
+
+tl.to("#poll", {
+    x: 900,
+    y: 300,
+    delay: 0.5,
+    duration: 1
+})
+
+tl.to("#net", {
+    x: 744,
+    y: 316,
+    duration: 1
+})
+
+
+tl.to("#ball", {
+    x: 700,
+    y: 600,
+    delay: 0.5,
+    duration: 1.5,
+})
+
+tl.to("#ball", {
+    x: 200,
+    y: 500,
+    duration: 1.5,
+})
+
+tl.to("#ball", {
+    x: 600,
+    y: 500,
+    duration: 1.5,
+})
+
+tl.to("#ball", {
+    x: 650,
+    y: 340,
+    duration: 1.5,
+})
+
+tl.from("#goal", {
+    opacity: 0,
+    duration: 0.8,
+    scale: 1.5
+})
