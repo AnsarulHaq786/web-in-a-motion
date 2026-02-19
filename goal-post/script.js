b5f55c09e250a47629c706da16a6064b8e1fc0db
user.name "Ansarul Haq <mdansarul786940@gmail.com>
2026-02-19 11:01:25 +0545

    fix: rotation of ball

--- goal-post/script.js
+++ goal-post/script.js
@@ -13,30 +13,33 @@ tl.to("#net", {
     duration: 1
 })
 
-
 tl.to("#ball", {
     x: 700,
     y: 600,
     delay: 0.5,
     duration: 1.5,
+    rotate: 1600
 })
 
 tl.to("#ball", {
     x: 200,
     y: 500,
     duration: 1.5,
+    rotate: 200
 })
 
 tl.to("#ball", {
     x: 600,
     y: 500,
     duration: 1.5,
+    rotate: 100
 })
 
 tl.to("#ball", {
     x: 650,
     y: 340,
     duration: 1.5,
+    rotate:200
 })
 
 tl.from("#goal", {
