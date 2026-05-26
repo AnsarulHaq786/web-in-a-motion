30997b52835a630a6fd10d10d03e6104c8e21b76
user.name "Ansarul Haq <mdansarul786940@gmail.com>
2026-05-26 15:45:47 +0545

    fix: add drag motion to ball and position of net

--- goal-post/script.js
+++ goal-post/script.js
@@ -1,49 +1,71 @@
-const tl = gsap.timeline();
+let BALL_START_X=(Math.random()*window.innerWidth+1)/3+window.innerWidth/4;
+let BALL_START_Y=(Math.random()*window.innerHeight+1)/2+window.innerHeight/4;
 
-tl.to("#poll", {
-    x: 900,
-    y: 300,
-    delay: 0.5,
-    duration: 1
-})
+let dragStartX, dragStartY;
+const line=document.getElementById('sling-line');
 
-tl.to("#net", {
-    x: 744,
-    y: 316,
-    duration: 1
-})
+gsap.set("#ball", { x: BALL_START_X, y: BALL_START_Y });
 
-tl.to("#ball", {
-    x: 700,
-    y: 600,
-    delay: 0.5,
-    duration: 1.5,
-    rotate: 1600
-})
+Draggable.create("#ball", {
+    type: "x, y",
+    inertia: true,
+    dragResistance: 0.75,
+    zIndexBoost: false,
 
-tl.to("#ball", {
-    x: 200,
-    y: 500,
-    duration: 1.5,
-    rotate: 200
-})
-
-tl.to("#ball", {
-    x: 600,
-    y: 500,
-    duration: 1.5,
-    rotate: 100
-})
-
-tl.to("#ball", {
-    x: 650,
-    y: 340,
-    duration: 1.5,
-    rotate:200
-})
-
-tl.from("#goal", {
-    opacity: 0,
-    duration: 0.8,
-    scale: 1.5
+    // bounds: {
+        
+    // },
+    onDragStart: function() {
+        dragStartX=this.x;
+        dragStartY=this.y;
+        gsap.to("#ball", { scale: 0.8, duration: 0.16 });
+        
+        console.log("drag started!");
+    },
+    onDrag: function() {
+        const ballEl=document.getElementById('ball');
+        const rect=ballEl.getBoundingClientRect();
+        
+        line.setAttribute("x1", BALL_START_X+24);
+        line.setAttribute("y1", BALL_START_Y+24);
+        line.setAttribute("x2", this.x+24);
+        line.setAttribute("y2", this.y+24);
+        
+        
+        console.log("drag dragging!");
+    },
+    onDragEnd: function() {
+        const dx=this.x-BALL_START_X;
+        const dy=this.y-BALL_START_Y;
+        
+        const dragDistance=Math.sqrt(dx*dx+dy*dy);
+        const MAX_DRAG=300;
+        
+        let power=Math.min(dragDistance/MAX_DRAG, 1);
+        const targetX=BALL_START_X-dx*3;
+        const targetY=BALL_START_Y-dy*3;
+ 
+        gsap.to("#ball", {
+            x: targetX,
+            y: targetY,
+            duration: 0.6*(1-power*0.4),
+            scale: 1,
+            ease: "power2out",
+            onComplete: () => {
+                // if goal, reset position and count goal
+                // to check goal, use get Rect of ball and poll
+                // set this position for next trigger until goal
+                BALL_START_X=targetX;
+                BALL_START_Y=targetY;
+                console.log("completed!!");
+                
+            }
+            
+        })
+        console.log("drag endeed!");
+        line.setAttribute("x1", 0);
+        line.setAttribute("x2", 0);
+        line.setAttribute("y1", 0);
+        line.setAttribute("y2", 0);
+    },
 })
