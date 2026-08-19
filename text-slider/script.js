19adab228b3dca8a2f29e71a51c810a5e1923c3a
user.name "Ansarul Haq <mdansarul786940@gmail.com>
2026-08-19 13:25:03 +0545

    text slider animation

---
+++ text-slider/script.js
@@ -0,0 +1,26 @@
+window.addEventListener("wheel", function(ele){
+    if(ele.deltaY>0) {
+        gsap.to(".marque", {
+            transform: 'translateX(-200%)',
+            duration: 2,
+            repeat: -1,
+            ease: "none"
+        })
+        
+        gsap.to(".marque i", {
+            rotate: 180
+        })
+    }
+    else {
+        gsap.to(".marque", {
+            transform: 'translateX(0%)',
+            duration: 2,
+            repeat: -1,
+            ease: "none"
+        })
+        
+        gsap.to(".marque i", {
+            rotate: 0
+        })
+    }
+})
